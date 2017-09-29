f56bdfbd46000bfa25cf377b51fb5394963a3f33
EpsWeb <daniilepel@gmail.com>
2017-09-29 23:05:46 +0300

    sorting added

--- Homeworks/Task2/src/ru/itpark/Main.java
+++ Homeworks/Task2/src/ru/itpark/Main.java
@@ -4,89 +4,120 @@ import java.util.Scanner;
 
 public class Main {
 
+
+    public static void showElements(int[] repos, int count) {
+        if (count == 0) {
+            System.out.println("Пустое хранилище!");
+        } else {
+            System.out.println("Ваш массив: ");
+            for (int i = 0; i < count; i++) {
+
+                System.out.print(repos[i] + " ");
+            }
+        }
+    }
+
+    public static int addToStart(int[] arr, int count, int number) {
+        if (count >= 100) {
+            System.out.println("Хранилище полное!");
+        } else {
+            for (int i = count; i > 0; i--) {
+                arr[i] = arr[i - 1];
+            }
+            arr[0] = number;
+            count++;
+        }
+        return count;
+    }
+
+    public static int addToEnd(int[] arr, int count, int number) {
+        if (count >= 100) {
+            System.out.println("Хранилище полное!");
+        } else {
+            arr[count] = number;
+            count++;
+        }
+        return count;
+    }
+
+    public static int deleteElem(int[] arr, int count, int input) {
+        if (input > count) {
+            System.out.println("У нас нет столько элементов");
+        } else {
+            if (count > 0) {
+                for (int i = input; i < count; i++) {
+                    arr[i] = arr[i + 1];
+                }
+                count--;
+            } else {
+                System.out.println("Массив пустой.");
+            }
+        }
+        return count;
+
+    }
+
+    public static void sortRepos(int[] repos, int count) {
+        for (int i = 1; i < count; i++) {
+            for (int n = 0; n < count - i; n++) {
+                if (repos[n] > repos[n + 1]) {
+                    swap(repos, n);
+                }
+            }
+        }
+    }
+
+    private static void swap(int[] repos, int n) {
+        int temp = repos[n];
+        repos[n] = repos[n + 1];
+        repos[n + 1] = temp;
+    }
+
     public static void main(String[] args) {
 
-//        1) Добавить в конец:
-//        Пользователь вводит число, вы это число добавляете в первый пустой элемент массива и увеличиваете count на 1.
-//                [ ] [ ] [ ]
-//>> 2
-//                [2] [ ] [ ]
-//>> 4
-//                [2] [4] [ ]
-//        2) Добавить в начало
-//                [2] [4] [ ]
-//>> 3
-//                [3] [2] [4]
-//        3) Удаление по номеру - пользователь вводит номер, вы смещаете все элементы, начиная со след. после текущего номера на 1 влево
-//                [3] [2] [4]
-//                >> 0
-//                [2] [4] [0], уменшить count!
+        int command, count = 0;
+        int repos[] = new int[100];
+
 
         System.out.println("1. Показать массив - выводит только count-элементов");
         System.out.println("2. Добавить в начало");
         System.out.println("3. Добавить в конец");
         System.out.println("4. Удалить по позиции");
-        System.out.println("5. Выход");
-
-        int command, count = 0;
-        int arr[] = new int[100];
+        System.out.println("5. Сортировать массив");
+        System.out.println("6. Выход");
 
         Scanner scn = new Scanner(System.in);
 
+
         while (true) {
-            System.out.println("Выберите пункт меню ");
             command = scn.nextInt();
-
             switch (command) {
-
                 case 1:
-                    System.out.println("Элементы массива: ");
-                    for (int i = 0; i < count; i++) {
-                        System.out.print(arr[i] + " ");
-                    }
+                    showElements(repos, count);
                     System.out.println();
                     break;
-
                 case 2:
-                    System.out.println("Введите элемент, который Вы хотите добавить в конец");
-                    int input = scn.nextInt();
-                    arr[count] = input;
-                    count++;
+                    System.out.println("Какой элемент Вы хотите добавить в начало?");
+                    count = addToStart(repos, count, scn.nextInt());
                     break;
-
                 case 3:
-                    System.out.println("Введите элемент, который Вы хотите добавить в начало");
-                    int input3 = scn.nextInt();
-                    if(count <= 100) {
-                        for (int i = count; i > 0; i--) {
-                            arr[i] = arr[i - 1];
-                        }
-                        arr[0] = input3;
-                        count++;
-                    } else {
-                        System.out.println("Больше нельзя!");
-                    }
+                    System.out.println("Какой элемент Вы хотите добавить в конец?");
+                    count = addToEnd(repos, count, scn.nextInt());
                     break;
-
                 case 4:
-                    if (count > 0) {
-                        System.out.println("Элемент какой позиции Вы хотите удалить?");
-                        int input4 = scn.nextInt();
-                        for (int i = input4; i < count; i++) {
-                            arr[i] = arr[i + 1];
-                        }
-                        count--;
-                    } else {
-                        System.out.println("Массив пустой.");
-                    }
+                    System.out.println("Какой элемент Вы хотите удалить?");
+                    count = deleteElem(repos, count, scn.nextInt());
                     break;
-
                 case 5:
+                    System.out.println("Отсортированное хранилище: ");
+                    sortRepos(repos, count);
+                    break;
+                case 6:
                     System.exit(0);
                     break;
-                    default:
-                        System.out.println("Комманда не определена");
-                        break;
+                default:
+                    System.out.println("Комманда не верна, братишь");
+                    break;
             }
 
         }
@@ -94,4 +125,3 @@ public class Main {
     }
 
 }
-
